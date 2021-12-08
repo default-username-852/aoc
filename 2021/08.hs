66bf6205750b0dd3e878599f5cc4701daf5aa36c
Eskil Queseth <eskilq@kth.se>
2021-12-08 12:44:46 +0100

    remove comment

--- 2021/08.hs
+++ 2021/08.hs
@@ -8,18 +8,6 @@ import Control.Monad
 
 type Indata = [([String], [String])]
 
-{-
-    1 -> 2
-    2 -> 5
-    3 -> 5
-    4 -> 4
-    5 -> 5
-    6 -> 6
-    7 -> 3
-    8 -> 7
-    9 -> 6
--}
-
 main :: IO ()
 main = do
     indata <- parseData <$> readFile "08.in"
